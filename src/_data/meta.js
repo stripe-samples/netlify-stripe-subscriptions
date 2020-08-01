40e4a6a860cea887d9d360e6653a5e7ae1c7e2d4
Ramin Sedighi <rsedighi@gmail.com>
2020-07-31 18:18:09 -0700

    Update meta.js
    Old link was broken. 
    
    Old Link: https://www.netlify.com/blog/2020/07/13/learn-how-to-add-subscriptions-and-protected-content-with-stripe/?utm_source=demo-footer&utm_medium=stripe-subs-jl&utm_campaign=devex
    
    New Link:  https://www.netlify.com/blog/2020/07/13/manage-subscriptions-and-protect-content-with-stripe?utm_source=demo-footer&utm_medium=stripe-subs-jl&utm_campaign=devex

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -7,5 +7,5 @@ module.exports = {
   episode:
     'https://www.learnwithjason.dev/subscription-management-in-jamstack-apps',
   tutorial:
-    'https://www.netlify.com/blog/2020/07/13/learn-how-to-add-subscriptions-and-protected-content-with-stripe/?utm_source=demo-footer&utm_medium=stripe-subs-jl&utm_campaign=devex',
+    'https://www.netlify.com/blog/2020/07/13/manage-subscriptions-and-protect-content-with-stripe?utm_source=demo-footer&utm_medium=stripe-subs-jl&utm_campaign=devex',
 };
